8a78f6b0fb622dbaedf851fd32072af19022d72b
Daniel André da Silva <dap1995@gmail.com>
2019-05-13 13:50:55 -0300

    Use enviroment variable to get secret key for guardian

--- config/config.exs
+++ config/config.exs
@@ -13,7 +13,8 @@ config :atlantis,
 # Configures the endpoint
 config :atlantis, AtlantisWeb.Endpoint,
   url: [host: "localhost"],
-  secret_key_base: "SnJzrcLI9EKYZWi3bw0IRA2eumZXs5m1ZUckdWi7PkNHFYUdFOqnrdF2tLl2AobP",
+  secret_key_base: System.get_env("SECRET_KEY_BASE") ||
+    "SnJzrcLI9EKYZWi3bw0IRA2eumZXs5m1ZUckdWi7PkNHFYUdFOqnrdF2tLl2AobP",
   render_errors: [view: AtlantisWeb.ErrorView, accepts: ~w(html json)],
   pubsub: [name: Atlantis.PubSub, adapter: Phoenix.PubSub.PG2]
 
@@ -29,7 +30,8 @@ config :phoenix, :json_library, Jason
 config :atlantis, Atlantis.Guardian,
   issuer: "atlantis",
   ttl: { 3, :days},
-  secret_key: "ycdxPBzzwGsZvTfpAvMJCGwgx1Xwsc80GZLNUo6ydBuLZCUPaBzD9YxjpM1YGfCD"
+  secret_key: System.get_env("SECRET_KEY_BASE") ||
+    "ycdxPBzzwGsZvTfpAvMJCGwgx1Xwsc80GZLNUo6ydBuLZCUPaBzD9YxjpM1YGfCD"
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
