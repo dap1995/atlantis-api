cd1ee3814a70ded3e747008d24649d1a1596b2e8
eduardosmatheus <matheus-edu@outlook.com>
2019-05-14 17:35:38 -0300

    Fix error on signup

--- lib/atlantis_web/controllers/user_controller.ex
+++ lib/atlantis_web/controllers/user_controller.ex
@@ -26,8 +26,7 @@ defmodule AtlantisWeb.UserController do
       {:ok, token, _claims} <- Guardian.encode_and_sign(user) do
       conn
       |> put_status(:created)
-      |> put_resp_header("location", Routes.user_path(conn, :show, user))
-      |> render("show.json", user: user)
+      |> render("jwt.json", jwt: token)
     end
   end
 
