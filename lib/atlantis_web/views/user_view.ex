2731438ded25b42013c8edc327264c09c95fdbc7
eduardosmatheus <matheus-edu@outlook.com>
2019-05-14 10:41:44 -0300

    Fix user view

--- lib/atlantis_web/views/user_view.ex
+++ lib/atlantis_web/views/user_view.ex
@@ -11,9 +11,10 @@ defmodule AtlantisWeb.UserView do
   end
 
   def render("user.json", %{user: user}) do
-    %{id: user.id,
-      email: user.email,
-      password_hash: user.password_hash}
+    %{
+      id: user.id,
+      email: user.email
+    }
   end
 
   def render("jwt.json", %{jwt: jwt}) do
